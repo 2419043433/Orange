273a784f6feb156cf9d5ff04e9eee0ccb1d67301
wangli <2419043433@qq.com>
2014-03-04 01:43:46 +0800

    add android project

---
+++ android_src/Orange/gen/R.java.d
@@ -0,0 +1,15 @@
+/home/orange/Projects/Orange/android_src/Orange/gen/com/orange/R.java \
+ : /home/orange/Projects/Orange/android_src/Orange/res/values-v11/styles.xml \
+/home/orange/Projects/Orange/android_src/Orange/res/values-v14/styles.xml \
+/home/orange/Projects/Orange/android_src/Orange/res/menu/main.xml \
+/home/orange/Projects/Orange/android_src/Orange/res/layout/activity_main.xml \
+/home/orange/Projects/Orange/android_src/Orange/res/drawable-hdpi/ic_launcher.png \
+/home/orange/Projects/Orange/android_src/Orange/res/values/strings.xml \
+/home/orange/Projects/Orange/android_src/Orange/res/values/styles.xml \
+/home/orange/Projects/Orange/android_src/Orange/res/values/dimens.xml \
+/home/orange/Projects/Orange/android_src/Orange/res/drawable-xhdpi/ic_launcher.png \
+/home/orange/Projects/Orange/android_src/Orange/res/values-sw720dp-land/dimens.xml \
+/home/orange/Projects/Orange/android_src/Orange/res/values-sw600dp/dimens.xml \
+/home/orange/Projects/Orange/android_src/Orange/res/drawable-xxhdpi/ic_launcher.png \
+/home/orange/Projects/Orange/android_src/Orange/res/drawable-mdpi/ic_launcher.png \
+/home/orange/Projects/Orange/android_src/Orange/bin/AndroidManifest.xml \
---
+++ build/README.md
@@ -0,0 +1,1 @@
+This folder contains all files used to build the whole system.
